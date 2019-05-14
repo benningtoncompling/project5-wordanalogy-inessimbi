db5dec47b35ef3db2c68d1d9b3cf62c1bf8e9539
inessimbi <inessimbi@bennington.edu>
2019-05-13 20:28:46 -0400

    updated word_analogy.py

--- word_analogy.py
+++ word_analogy.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 #
-# word_analogy.py - building
+# word_analogy.py - building word analogy solver
 # Author: <Ines Simbi>(inessimbi@bennington.edu)
-# Date: <05.05.2019>./word_analogy.py <vector_file> <input_directory> <output_directory> <eval_file>
-# <should_normalize> <similarity_type>
+# Date: <05.05.2019>
 
-
-
-#import sys
 import sys, os
 import numpy as np
-import gensim
-from gensim.models import Word2Vec
 
 file_name = sys.argv[1]
 google_test_set = sys.argv[2]
 out_file_name = sys.argv[3]
-output_eval_file = sys.argv[4]
-
-
-file_name = "smaller_model.txt"
-out_file_name = "output_directory"
-output_eval_file = "eval.txt"
-google_test_set = "GoogleTestSet"
+outfile_eval_file = sys.argv[4]
+#file_name = "smaller_model.txt"
+#out_file_name = "output_directory"
+#outfile_eval_file = "output_eval.txt"
+#google_test_set = "GoogleTestSet"
 
 
 words_dict = {}
@@ -32,19 +24,19 @@ with open(file_name, 'r', encoding="utf-8") as in_file:
 			the_line = line.split(" ")
 			the_key = the_line[0]
 			value = np.array(the_line[1:]) # getting values of the key
-			words_dict[the_key] = value
-
-
 
 def find_vector(the_file_path, should_normalize, similarity_type):
-	for filename in os.listdir(google_test_set):
-		for file_name in os.listdir(out_file_name):
-			if filename.startswith('.') or file_name.startswith('.'):
+	eval_path = os.path.join(outfile_eval_file,"output_eval.txt")
+	with open(eval_path, 'w', encoding="utf-8") as output_eval_file:
+		match_counter = 0
+		negative_match_counter = 0
+		for filename in os.listdir(google_test_set):
+			if filename.startswith('.'):
 				continue
-			if not filename.endswith('.zip') and file_name.endwith(' '):
+			if not filename.endswith('.txt'):
 				continue
 			the_file_path = os.path.join(google_test_set, filename)
-			file_path = os.path.join(out_file_name, file_name)
+			file_path = os.path.join(out_file_name, filename)
 			with open(the_file_path, 'r', encoding="utf-8") as open_file:
 				with open(file_path, 'w', encoding="utf-8") as out_put_file:
 					for line in open_file.readlines():
@@ -59,6 +51,7 @@ def find_vector(the_file_path, should_normalize, similarity_type):
 							vec_c = vec_c/np.linalg.norm(vec_c)
 						the_estimate = vec_c + vec_b - vec_a
 						temp_smallest = 1000 #for testing
+						temp_max = -10000000000
 						key_curr_candidate = " "
 						distance = 0
 						for key in words_dict.keys():
@@ -68,22 +61,38 @@ def find_vector(the_file_path, should_normalize, similarity_type):
 								distance = manhattan_similarity(the_estimate, words_dict[key])
 							else:
 								distance = cosine_similarity(the_estimate, words_dict[key])
-							if distance < temp_smallest:
+							if distance < temp_smallest and similarity_type < 2:
 								temp_smallest = distance
 								key_curr_candidate = key
-						out_put_file.write(a_line[0])
-						out_put_file.write(a_line[1])
-						out_put_file.write(a_line[2])
-						out_put_file.write(key_curr_candidate)
+							if distance > temp_max and similarity_type == 2:
+								temp_max = distance
+								key_curr_candidate = key
+						out_put_file.write(a_line[0], " ")
+						out_put_file.write(a_line[1], " ")
+						out_put_file.write(a_line[2], " ")
+						out_put_file.write(key_curr_candidate, " ", "\n")
+
+						if key_curr_candidate == a_line[3]:
+							match_counter += 1
+						else:
+							negative_match_counter +=1
+
+			sum = match_counter + negative_match_counter
+			accuracy_checker_correct = match_counter/sum
+			print("(", str(match_counter), "/", str(sum))
+			output_eval_file.write(filename, "/n")
+			output_eval_file.write(accuracy_checker_correct, "%", " ", "(", str(match_counter), "/", str(sum), ")", "\n")
 
 
 def euclidean_similarity(vec1, vec2):
 	return np.sqrt(np.sum((vec1-vec2)**2, axis=1)) #taking elements of vec 1 and cev element
 
 def manhattan_similarity(vec1, vec2):
-	return np.sqrt(np.sum(np.absolute(vec1-vec2), axis=1))  # got the formula from numoy.org and docs.scipy.org
+	return np.sum(np.absolute(vec1-vec2), axis=1)  # got the formula from numoy.org and docs.scipy.org
 def cosine_similarity(vec1, vec2):
 	return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2)) #got it from program creek
-#def accuracy(vec1, vec2):
+
+
+
 
 
